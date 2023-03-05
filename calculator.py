10764d4f9686035ed16a342117c31a627721490a
zakariamiti <zakariamiti@gmail.com>
2023-03-06 00:38:03 +0100

    my 1st program paython

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("Calculator")
+        A = float(input("Please enter the 1st number: "))
+        o = str(input("Please choice your operator : + ; - ; * or / ==>  "))
+        B = float(input("Please enter the 2sd number: "))
+        if o == '+':
+                print("result is : ", A + B)
+            elif o == '-':
+                        print("result is : ", A - B)
+                    elif o == '*':
+                                    print("result is : ", A * B)
+                                elif o == '/':
+                                                    print("result is : ", A / B )
+                                                else: print("ERROR")print("Calculator")
+                                                A = float(input("Please enter the 1st number: "))
+                                                o = str(input("Please choice your operator : + ; - ; * or / ==>  "))
+                                                B = float(input("Please enter the 2sd number: "))
+                                                if o == '+':
+                                                        print("result is : ", A + B)
+                                                    elif o == '-':
+                                                                print("result is : ", A - B)
+                                                            elif o == '*':
+                                                                            print("result is : ", A * B)
+                                                                        elif o == '/':
+                                                                                            print("result is : ", A / B )
+                                                                                        else: print("ERROR")
