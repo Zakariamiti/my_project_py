44ac5244fc0e2468a70cb520913d8085775d8700
zakariamiti <zakariamiti@gmail.com>
2023-03-06 01:17:24 +0100

    editing script

--- calculator.py
+++ calculator.py
@@ -10,16 +10,4 @@ print("Calculator")
                                     print("result is : ", A * B)
                                 elif o == '/':
                                                     print("result is : ", A / B )
-                                                else: print("ERROR")print("Calculator")
-                                                A = float(input("Please enter the 1st number: "))
-                                                o = str(input("Please choice your operator : + ; - ; * or / ==>  "))
-                                                B = float(input("Please enter the 2sd number: "))
-                                                if o == '+':
-                                                        print("result is : ", A + B)
-                                                    elif o == '-':
-                                                                print("result is : ", A - B)
-                                                            elif o == '*':
-                                                                            print("result is : ", A * B)
-                                                                        elif o == '/':
-                                                                                            print("result is : ", A / B )
-                                                                                        else: print("ERROR")
+                                                else: print("ERROR")
